efdffa92aac9063559332cd40389ec9b7a2d261f
Lynx82 <ristanovski@hotmail.com>
2022-02-03 09:41:28 +0100

    Add project files.

---
+++ Win_The_Lottery/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Win_The_Lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the desired characters:");
+            string input = Console.ReadLine();
+
+            Algorithms permAlgorithm = new Algorithms();
+            permAlgorithm.InputSet = permAlgorithm.MakeCharArray(input);
+
+            permAlgorithm.Recursion(0);
+            Console.Write("# of Permutations: " + permAlgorithm.PermutationCount);
+
+            Console.ReadKey();
+        }
+    }
+}
